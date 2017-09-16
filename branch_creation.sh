4acfcc4bacaa19b240d3bd4d9d3c92033e7c1a0d
smishra1985 <smishra1985@gmail.com>
2017-09-16 16:53:54 -0700

    initial commit

---
+++ branch_creation.sh
@@ -0,0 +1,46 @@
+# creates a remote git repository from the current local directory
+
+# Configuration
+# Replace SSH_USERNAME, SSH_HOST, SSH_GIT_PATH with your details
+USER=smishra1985
+HOST=git@github.com
+GIT_PATH=git@github.com:smishra1985/Repo_15_09.git
+
+REPO=${PWD##*/}
+GIT_REMOTE_URL=ssh://$USER@$HOST/$GIT_PATH/$REPO
+
+echo "-------------------------------------------"
+echo "------ Building New Git Repository --------"
+echo "-------------------------------------------"
+
+# Setup remote repo
+
+echo "--"
+echo "-- Creating bare remote repo at:"
+echo "-- $USER@$HOST/$GIT_PATH/$REPO"
+echo "--"
+
+ssh $USER@$HOST 'mkdir '$GIT_PATH'/'$REPO' && cd '$GIT_PATH'/'$REPO' && git --bare init && git --bare update-server-info && cp hooks/post-update.sample hooks/post-update && chmod a+x hooks/post-update && touch git-daemon-export-ok'
+
+# Configure local repo
+
+echo "--"
+echo "-- Initializing local repo & pushing to remote"
+echo "--"
+
+touch .gitignore
+git init
+git add .
+git commit -m 'initial commit'
+git push --all $GIT_REMOTE_URL
+git remote add origin $GIT_REMOTE_URL
+git config branch.master.remote origin
+git config branch.master.merge refs/heads/master
+git fetch
+git merge master
+git branch -a
+
+echo "--"
+echo "-- Your new git repo '$REPO' is ready and initialized at:"
+echo "-- $USER@$HOST/$GIT_PATH/$REPO"
+echo "--"
